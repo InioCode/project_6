3ae05b77da129825b8e714e98e8a325b8839d569
Nikolai Kudryashov <kolxx199@ya.ru>
2024-03-14 10:50:53 +0400

    add tests for Feline

---
+++ src/test/java/TestFeline.java
@@ -0,0 +1,48 @@
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import org.junit.Assert;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestFeline {
+
+    @Spy
+    Feline spyFeline;
+
+   @Test
+    public void getMeatCallGetFoodWithReturnAnimalKind(){
+       try {
+           spyFeline.eatMeat();
+       } catch (Exception e) {
+           throw new RuntimeException(e);
+       }
+
+       try {
+           Mockito.verify(spyFeline, Mockito.times(1)).getFood("Хищник");
+       } catch (Exception e) {
+           throw new RuntimeException(e);
+       }
+
+   }
+
+   @Test
+    public void getFamilyReturnCats(){
+       Assert.assertEquals("Кошачьи",spyFeline.getFamily());
+   }
+
+   @Test
+    public void getKittensWithThreeReturnThree(){
+       spyFeline.getKittens(3);
+       Mockito.verify(spyFeline,Mockito.times(1)).getKittens(3);
+   }
+
+   @Test
+    public void getKittensWithoutNumberReturnOne(){
+       spyFeline.getKittens();
+       Mockito.verify(spyFeline,Mockito.times(1)).getKittens(1);
+   }
+}
